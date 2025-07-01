3c8a52cf741719430b4cc01c0fdac8da2733b109
Subramanya Malleshi <SMalleshi@jmawireless.com>
2025-07-01 08:52:14 +0530

    landing page

--- client/src/app/modules/landing/home/home.component.ts
+++ client/src/app/modules/landing/home/home.component.ts
@@ -8,7 +8,7 @@ import { RouterLink } from '@angular/router';
     templateUrl: './home.component.html',
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [MatButtonModule, RouterLink, MatIconModule],
+    imports: [MatButtonModule, MatIconModule],
 })
 export class LandingHomeComponent {
     /**
